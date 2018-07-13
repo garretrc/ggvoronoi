5ee0cbe49b390c8a532723df33b895a42dd1ecde
garretrc <garretrc@miamioh.edu>
2018-07-13 12:45:39 -0400

    added ncdc data

--- R/data.R
+++ R/data.R
@@ -9,4 +9,21 @@
 #'   \item{name}{location name, if recorded}
 #' }
 #' @source Madeline Maurer, Justin Hopkins, Dr. Helaine Alessio, Amanda Meiering
-"bikes"
+"oxford_bikes"
+
+#' Locations and Elevation of NCDC Weather Stations
+#'
+#' A dataset containing identifying information as well as coordinates and elevation for each unique land-based weather station in the National Climatic Data Center.
+#' Note that sites with a missing elevation in the raw data (recorded as -999.9) have been removed.
+#'
+#' @format A data frame with 65804 rows and 6 variables:
+#' \describe{
+#'   \item{ID}{ID number to connect the data back to other NCDC data}
+#'   \item{country}{Country that the site is in. US, Canada or Mexico}
+#'   \item{state}{state/province that the site is in}
+#'   \item{lat}{latitude coordinate}
+#'   \item{long}{longitude coordinate}
+#'   \item{elev}{site elevation in meters}
+#' }
+#' @source https://www.ncdc.noaa.gov/data-access/land-based-station-data
+"ncdc_locations"
